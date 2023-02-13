4f514c5dfd41f503c9599ec27afa530d50d30964
unknown <danielcely1@gmail.com>
2023-02-13 13:24:38 -0500

    inicio sequelice PI

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -4,9 +4,44 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('country', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: sequelize.UUIDV4,
+      primaryKey: true,
+      allowNull: false
+    },
+
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+
+    continent:{
+      type: DataTypes.STRING,
+      allowNull:false,
+    },
+
+    capital:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    subregion:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    area:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    population:{
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+
+
   });
+  
 };
