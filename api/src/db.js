22835db9a35e4bccce34aa205ff0d588762dc329
Daniel Yesid Cely Robayo <danielcely1@gmail.com>
2023-02-15 17:49:44 -0500

    avance api

--- api/src/db.js
+++ api/src/db.js
@@ -10,9 +10,12 @@ const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
+
+
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
+
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
 fs.readdirSync(path.join(__dirname, '/models'))
@@ -34,6 +37,9 @@ const { Pokemon } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
+
+
+
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
