83e580d21d0f4e4c7ccddb5e4e9aa3b21740b9fd
Daniel Yesid Cely Robayo <danielcely1@gmail.com>
2023-02-17 20:44:06 -0500

    correccion constantes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,16 +1,12 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+const axios = require('axios');
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
-router.get('/', (req, res)=>{
-  res.send('countries')
-})
 
   const countries = async ()=>{
     const dApi = await axios.get(`https://restcountries.com/v3/all`)
