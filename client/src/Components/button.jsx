ac50736be3ec2cdfa0f0a48bc17c7f55d4036365
Daniel Yesid Cely Robayo <danielcely1@gmail.com>
2023-02-27 01:00:21 -0500

    inicio frontEnd

---
+++ client/src/Components/button.jsx
@@ -0,0 +1,7 @@
+
+export default function Botton(){
+
+  return (<div>
+    Ingresar
+  </div>)
+}
---
+++ client/src/Components/start.jsx
@@ -0,0 +1,7 @@
+import Botton from "./button"
+export default function Start(){
+
+  return(<div className="bkg_start">
+    <Botton />
+  </div>)
+}
