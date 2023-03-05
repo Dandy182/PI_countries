61bb003bc8365920a6b8e6e00ca556ed1ec43d5d
Daniel Yesid Cely Robayo <danielcely1@gmail.com>
2023-03-04 19:14:35 -0500

    modificacion de home

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import {useState, useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCountries} from '../actions/index';
+import {Link} from 'react-router-dom'
 
 export default function Home(){
 
@@ -11,8 +13,20 @@ export default function Home(){
 
     useEffect (()=>{
         dispatch(getCountries());
-    }, [dispatch])
-    return(<div>
+    }, [])
+
+    function handleButton(e){
+        e.preventDefault();
         
+    }
+
+
+    
+    return(<div>
+        <Link to='/characters'> Crear Actividad</Link>
+
+        <h1>Countries</h1>
+
+        <button>See All Countries</button>
     </div>)
 }
